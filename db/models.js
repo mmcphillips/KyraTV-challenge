51ad3e024e395d7bcca5d7a198e57f6c0108a4c7
Matthew McPhillips <matthewcmcphillips@gmail.com>
2019-12-27 16:30:19 -0800

    Add more descriptive comments

--- db/models.js
+++ db/models.js
@@ -1,6 +1,9 @@
+// methods for manipulating database entries
 require('dotenv').config();
+// pg-promise offers performant way to multi line insert for large queries using transactions.
 const pgp = require('pg-promise')({ capSQL: true });
 
+// psql configuration object
 const db = pgp({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
@@ -9,6 +12,7 @@ const db = pgp({
   port: process.env.PGPORT,
 });
 
+// column set names for uploading bulk entries with pg-promise
 const csUploads = new pgp.helpers.ColumnSet([
   'channelid',
   'channelname',
@@ -22,9 +26,8 @@ const csUploads = new pgp.helpers.ColumnSet([
   'tmax',
 ], { table: 'uploads' });
 
+// create single upload objects with pertinent information to update into db.
 const createUploadObject = function (item) {
-  // create single upload objects with pertinent information to update into db.
-  // pg-promise offers performant way to multi line insert.
   return {
     channelid: item.channelid,
     channelname: item.channelname,
@@ -42,14 +45,12 @@ const createUploadObject = function (item) {
 module.exports = {
   // return existing records from database based on channel id
   get(params) {
-    // const selectQuery = `SELECT * FROM uploads where channelname = ?`;
-    // db.one(selectQuery, params)
     return db.many({
       text: 'SELECT * FROM uploads WHERE channelname = $1',
       values: params,
     });
   },
-  // add new records to the database
+  // add new records to the database in bulk insertion queries
   async update(videoList) {
     const uploadsObjects = videoList.map((video) => createUploadObject(video));
     // in instance where there are conflicts, do not insert.
