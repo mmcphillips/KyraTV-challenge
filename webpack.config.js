9e6314ccdfa0d98d47faf161d37d14f4c1feb112
Matthew McPhillips <matthewcmcphillips@gmail.com>
2019-12-27 16:34:43 -0800

    Add comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const webpack = require('webpack');
 const dotenv = require('dotenv');
-
+// webpack configuration object for bundling assets and exposing .env variables to client
 module.exports = () => {
   const env = dotenv.config().parsed;
   const envKeys = Object.keys(env).reduce((prev, next) => {
     prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
   }, {});
-
+  // webpack configuration object defining entry point, what files to bundle, and which to exclude
   return {
     entry: `${__dirname}/client/src/index.jsx`,
     module: {
@@ -31,6 +31,7 @@ module.exports = () => {
     node: {
       fs: 'empty',
     },
+    //custom plugin to expose .env to client
     plugins: [
       new webpack.DefinePlugin(envKeys),
     ],
